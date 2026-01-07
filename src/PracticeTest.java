7efb6c046326dd0e23abefa75cd1a29cc979822f
Lilian <210286008+liliann19@users.noreply.github.com>
2026-01-07 13:47:33 -0800

    added test for mostCommonTimeEfficient

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -7,4 +7,11 @@ public class PracticeTest {
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
+
+  @Test
+    void testMostCommonTimeEfficient(){
+    int[] nums = {1,2,4,5,5,6,6};
+    int actual = Practice.mostCommonTimeEfficient(nums);
+    assertEquals(5, actual);
+}
 }
